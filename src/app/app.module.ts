fde9c30e398c6432a2988864cc08e280124c5dfc
johan1995 <johan.aylas@unmsm.edu.pe>
2018-05-06 01:08:17 -0500

    template admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserComponent } from './user/user.component';
+import { AppheaderComponent } from './componentes/appheader/appheader.component';
+import { AppsettingsComponent } from './componentes/appsettings/appsettings.component';
+import { AppmenuComponent } from './componentes/appmenu/appmenu.component';
+import { AppfooterComponent } from './componentes/appfooter/appfooter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AppheaderComponent,
+    AppsettingsComponent,
+    AppmenuComponent,
+    AppfooterComponent
   ],
   imports: [
     BrowserModule,
